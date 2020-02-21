77cbe61832b14b64d2d0fdd0fcf144f3597c2d02
jthanawat <kla13thanawat@gmail.com>
2020-02-21 16:35:49 +0700

    [Added] Exercise 1 and 2

---
+++ index.js
@@ -0,0 +1,99 @@
+// let user = {
+//     name: "Thanawat",
+//     age: 30,
+//     "computer skill": null
+// };
+
+// user.height = 160;
+// user.IsSingle = true;
+// console.log(user);
+// console.log(user["computer skill"]);
+// alert(user.name);
+
+// delete user.name
+// console.log(user);
+
+//Exercise 
+// let human = {};
+
+// human.name = "Thanawat",
+// human.age = 24,
+// human.group = "Nat",
+// human.IsSingle = true,
+// human["Intelligent Score"] = 4
+
+// console.log(human);
+
+//Exercise
+
+// let obj = {};
+// let key;
+// let value;
+
+
+// while (key !== "stop" || value !== "stop") {
+//   key = prompt("Enter key")
+//   if (key == "stop") {
+//     break;
+//   }
+//     value = prompt("Enter value")
+//     if (value == "stop") {
+//       break;
+//     }
+//   obj[key] = value;
+// }
+
+// console.log(obj);
+
+// เสริม
+// let key = prompt("Key");
+// let value = prompt("Value");
+
+// let obj = {};
+
+// obj[key] = value;
+// obj["age"] = 19;
+
+// console.log(obj);
+
+// let obj = {};
+
+// while(true) {  
+//   let propertyName = prompt("Enter property's Name");
+//   if (propertyName == "stop") break;
+//   let propertyValue = prompt("Enter property's Value");
+//   obj[propertyName] = propertyValue;
+// }
+
+// console.log(obj);
+
+// let v1 = "firstName"
+// let user = {};
+
+
+// user[v1] = "Thanawat";
+// user["lastName"] = "J";
+// user["age"] = 24;
+// user["height"] = 160;
+
+
+// Exercise 2
+
+let fruit = {};
+let key;
+let value;
+
+while(true) {
+  key = prompt("Enter fruit's name");
+  if (key == "stop") {
+    break;
+  } 
+    value = Number(prompt("Enter fruit's amount"));
+    if (value > 1) {
+      fruit[key + "s"] = value;
+      console.log(fruit);
+    } else {
+      fruit[key] = value;
+      console.log(fruit);
+    }
+} 
